daf75f763ccb93da2d88033a2e0d7a049e5cb721
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2023-11-26 15:19:35 +0100

    Correcting collection directory

--- front-end/src/Pages/Services/Services.jsx
+++ front-end/src/Pages/Services/Services.jsx
@@ -4,7 +4,7 @@ import Navbar from '../Homepage/Sections/Navbar'
 import Products from './Sections/Products'
 import Hero from './Sections/Hero'
 import Book from './Sections/Book'
-import Collection from './Sections/collection'
+import Collection from "./Sections/Collection";
 import Featured from './Sections/Featured'
 import Stories from './Sections/Stories'
 import Footer from '../LandingPage/Sections/Footer'
